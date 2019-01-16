4702cfeecf3ca6a3e2a55cf99081487f4c112989
Joel C <jtcofie@gmail.com>
2019-01-16 14:43:13 +0000

    additional updates and changes to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,35 @@ import { MatToolbarModule,
   MatButtonModule,
   MatSidenavModule
  } from '@angular/material';
- import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
- import {MatIconModule} from '@angular/material/icon';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImgCardComponent } from './img-card/img-card.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgCardComponent
+    ImgCardComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
+    MatListModule,
+    MatGridListModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
